2b13910b62a3855b3f40cb2c87b94bc384b6e52b
NguyenHuuNam3 <nhnam4411@gmail.com>
2025-07-22 19:42:59 +0700

    fix access token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -32,15 +32,15 @@ export class AuthService {
 
     res.cookie('access_token', accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 1 * 60 * 60 * 1000,
     });
 
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
